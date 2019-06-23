add3f7889859911905d308a7e476fbe63afce2aa
godiale <godiale@godiale.com>
2019-06-23 20:53:09 +0100

    Start primes generation from saved point (no tests yet)

--- src/prime/PrimeGenerator.java
+++ src/prime/PrimeGenerator.java
@@ -1,5 +1,6 @@
 package prime;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,14 +27,31 @@ public class PrimeGenerator {
         return nextCandidate;
     }
 
-    public static void main(String[] args) {
+    public static int MAX_PRIME_INDEX = 1000000;
+    public static String PRIMES_FILE_NAME = "primes.txt";
+
+    public static void main(String[] args) throws IOException {
         List<Long> primesList = new ArrayList<>();
-        for (int i=1; i <= 100000; i++) {
-            long prime = findNextPrime(primesList);
-            primesList.add(prime);
-            System.out.print(Long.toString(prime) + ",");
-            if (i % 20 == 0) {
-                System.out.println("");
+        File primesFile = new File(PRIMES_FILE_NAME);
+        if (primesFile.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(primesFile))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    primesList.add(Long.valueOf(line));
+                }
+            }
+        }
+        System.out.println(String.format("Starting generation from %d of %d",
+                primesList.size() + 1, MAX_PRIME_INDEX));
+        try (Writer writer = new BufferedWriter(new FileWriter(primesFile))) {
+            for (int i = primesList.size() + 1; i <= MAX_PRIME_INDEX; i++) {
+                long prime = findNextPrime(primesList);
+                primesList.add(prime);
+                writer.write(Long.toString(prime)+"\n");
+                if (i % 1000 == 0) {
+                    System.out.print(".");
+                    writer.flush();
+                }
             }
         }
     }
